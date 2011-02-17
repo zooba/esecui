567414e5ee99e1146fb9f8b7cf6078e215f58927
Steve Dower <steve@stevedower.id.au>
2011-02-18 08:41:50 +1100

    Updated pylib to minimum required from CPython 2.7

--- pylib/abc.py
+++ pylib/abc.py
@@ -3,6 +3,13 @@
 
 """Abstract Base Classes (ABCs) according to PEP 3119."""
 
+import types
+
+
+# Instance of old-style class
+class _C: pass
+_InstanceType = type(_C())
+
 
 def abstractmethod(funcobj):
     """A decorator indicating abstract methods.
@@ -96,7 +103,7 @@ class ABCMeta(type):
 
     def register(cls, subclass):
         """Register a virtual subclass of an ABC."""
-        if not isinstance(cls, type):
+        if not isinstance(subclass, (type, types.ClassType)):
             raise TypeError("Can only register classes")
         if issubclass(subclass, cls):
             return  # Already a subclass
@@ -124,6 +131,9 @@ class ABCMeta(type):
         if subclass in cls._abc_cache:
             return True
         subtype = type(instance)
+        # Old-style instances
+        if subtype is _InstanceType:
+            subtype = subclass
         if subtype is subclass or subclass is None:
             if (cls._abc_negative_cache_version ==
                 ABCMeta._abc_invalidation_counter and
