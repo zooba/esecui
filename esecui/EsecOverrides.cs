6ed4c22584ed91c17df4189fe0fe44fad8a6819a
Steve Dower <steve@stevedower.id.au>
2011-03-07 11:19:44 +1100

    Updated esec

--- esecui/EsecOverrides.cs
+++ esecui/EsecOverrides.cs
@@ -9,11 +9,12 @@ namespace esecui
     {
         public static void AddOverrides(dynamic esec)
         {
-            esec.species._pairs = new Func<object, IEnumerable<object>>(_Pairs);
+            esec.utils.pairs = new Func<object, IEnumerable<object>>(Pairs);
+            esec.utils.overlapped_pairs = new Func<object, IEnumerable<object>>(OverlappedPairs);
             esec.compiler.ExceptionGroup = new Func<string, IEnumerable<dynamic>, object>(ExceptionGroup);
         }
         
-        public static IEnumerable<object> _Pairs(object source)
+        public static IEnumerable<object> Pairs(object source)
         {
             object[] objs = new object[2];
             System.Collections.IEnumerator e;
@@ -26,6 +27,23 @@ namespace esecui
                 if (!e.MoveNext()) break;
                 objs[1] = e.Current;
                 yield return new PythonTuple(objs);
+            }
+        }
+
+        public static IEnumerable<object> OverlappedPairs(object source)
+        {
+            object[] objs = new object[2];
+            System.Collections.IEnumerator e;
+            if (source is System.Collections.IEnumerator) e = (System.Collections.IEnumerator)source;
+            else e = ((System.Collections.IEnumerable)source).GetEnumerator();
+            if (!e.MoveNext()) yield break;
+            objs[0] = e.Current;
+            while (true)
+            {
+                if (!e.MoveNext()) break;
+                objs[1] = e.Current;
+                yield return new PythonTuple(objs);
+                objs[0] = objs[1];
             }
         }
 
